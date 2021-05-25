7a29bdcd2294770133a15998b0ebfb66578e58f6
rjluzardo <rjlsyscripciones@gmail.com>
2021-05-25 18:24:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def predict():
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
     return jsonify({'prediction': prediction})
-    LOG.info("Output prediction: {prediction}")
+    #LOG.info("Output prediction: {prediction}")
 
 if __name__ == "__main__":
     # load pretrained model as clf
