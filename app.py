319637447d8ce55cdacc7345b823e8d76407a4e0
rjluzardo <rjlsyscripciones@gmail.com>
2021-06-15 15:06:19 -0500

    fixing logging errors

--- app.py
+++ app.py
@@ -9,19 +9,18 @@ from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
-logging.basicConfig(filename='docker_out.txt',level=logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
     
-    LOG.info("Scaling Payload: \n{payload}")
+    LOG.info(f"Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
+    html = f"<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -55,19 +54,17 @@ def predict():
         """
     
     # Logging the input payload
-    
     json_payload = request.json
-    LOG.info("JSON payload: \n{json_payload}")
+    LOG.info(f"JSON payload: \n{json_payload}")
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info("Inference payload DataFrame: \n{inference_payload}")
-    
+    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
     # scale the input
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+    LOG.info(f"prediction: {prediction}")
     return jsonify({'prediction': prediction})
-    #LOG.info("Output prediction: {prediction}")
 
 if __name__ == "__main__":
     # load pretrained model as clf
